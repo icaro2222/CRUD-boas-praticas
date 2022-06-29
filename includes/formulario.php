fcecf20f65a5ee0c0201a5ab5d6d1fa44155b0fc
icaro2222 <icarodias2222@gmail.com>
2022-06-28 23:07:27 -0300

    .

--- includes/formulario.php
+++ includes/formulario.php
@@ -1,34 +1,47 @@
-<main>
+<main></main>
+    <section>
+      <a href="index.php">
+        <button class="btn btn-success">Voltar</button>
+      </a>
+    </section>
 
-  <section>
-    <a href="index.php">
-      <button class="btn btn-success">Voltar</button>
-    </a>
-  </section>
+    <h2 class="mt-3"><?=TITLE?></h2>
 
-  <h2 class="mt-3"><?=TITLE?></h2>
+    <form method="post">
 
-  <form method="post">
+<div class="container-fluid">
+<div class="row">
+      <div class="col-md-7">
+        <label>Nome</label>
+        <input type="text" class="form-control" name="nome" value="<?=$obAluno->nome?>">
+      </div>
 
-    <div class="form-group">
-      <label>Nome</label>
-      <input type="text" class="form-control" name="nome" value="<?=$obAluno->nome?>">
+    <div class="col-md-5">
+      <label>Endereço</label>
+      <textarea class="form-control" name="endereco"><?=$obAluno->endereco?></textarea>
     </div>
 
-  <div class="form-group">
-    <label>Endereço</label>
-    <textarea class="form-control" name="endereco"><?=$obAluno->endereco?></textarea>
-  </div>
-
-  <div class="form-group">
-    <label>Nota</label>
-    <textarea class="form-control" name="nota"><?=$obAluno->nota?></textarea>
-  </div>
-
-    <div class="form-group">
-      <button type="submit" class="btn btn-success">Enviar</button>
+    <div class="col-md-2">
+      <label>Nota</label>
+      <textarea class="form-control" name="nota"><?=$obAluno->nota?></textarea>
     </div>
 
-  </form>
+    <div class="col-md-7">
+      <label>Anotação</label>
+      <textarea class="form-control" name="nota"><?=$obAluno->nota?></textarea>
+    </div>
+
+    <div class="col-md-3">
+      <label>Anotação</label>
+      <textarea class="form-control" name="nota"><?=$obAluno->nota?></textarea>
+    </div>
+
+      <div class="col-md-1">
+        <button type="submit" class="btn btn-success">Enviar</button>
+      </div>
+  </div>
+</div>
+
+    </form>
 
 </main>
